20f65e4a7e0b3333fa694eaff307406acc140769
engnakagawa <nakagawa@engraphia.com>
2018-07-23 18:44:44 +0900

    なんかいろいろ

--- .zsh.d/auto_notify.sh
+++ .zsh.d/auto_notify.sh
@@ -23,7 +23,7 @@ function precmd_function() {
 	time=`expr $END_TIME - $BEGIN_TIME`
 	if [ ${AUTO_NOTIFY_TIME:-3} -lt $time ];then
 		if [ "${AUTO_NOTIFY_COMMAND:-UNDEF}" = "UNDEF" ];then
-			terminal-notifier -sound default -message "$precmd" -group owata > /dev/null
+			terminal-notifier  -title "${precmd}" -message "${time} sec." -sound default -group owata > /dev/null
 		fi
 	fi
 }
